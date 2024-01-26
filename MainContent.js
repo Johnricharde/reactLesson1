d21629e9241d584e3ddaae269103c967878d2645
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-26 14:25:10 +0100

    Organized components
    Organized the components into their own files

---
+++ MainContent.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+
+                <li>I can begin working on my personal project</li>
+                <li>I can learn something new</li>
+                <li>I can be more efficient by using React</li>
+            </ol>
+        </div>
+    )
+}
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /**
-Challenge: 
-
-- Add an `ul` inside the Header's `nav` and create
-  the following `li`s: "Pricing", "About", & "Contact"
-- Using flexbox, line up the nav items horizontally, and
-  put them inline with the React logo.
+Challenge: move the Footer and MainContent components
+into their own files.
 */
+import Header from "./Header.js"
+import MainContent from "./MainContent.js"
+import Footer from "./Footer.js"
 
-function Page() {
+function App() {
     return (
         <div>
             <Header />
@@ -17,43 +16,4 @@ function Page() {
     )
 }
 
-function Header() {
-    return (
-        <header>
-            <nav className="nav">
-                <img className="nav-img" src="./react-logo.png" alt="React Logo" />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-
-                <li>I can begin working on my personal project</li>
-                <li>I can learn something new</li>
-                <li>I can be more efficient by using React</li>
-            </ol>
-        </div>
-    )
-}
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2021 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-
-
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
