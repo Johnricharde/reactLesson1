e5e89c24e6348aeec70d93420db1d8b4e2d14323
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-28 12:41:28 +0100

    started over again

--- reactLesson1Project/components/Navbar.js
+++ reactLesson1Project/components/Navbar.js
@@ -1,7 +1,20 @@
+/**
+Challenge: Complete the Navbar to match the design
+
+Hint: use the Figma file for the most accurate peek at the design
+(colors, sizes, fonts, etc.)
+*/
+
 import React from "react"
 
 export default function Navbar() {
     return (
-        <h1>Navbar</h1>
+        <div className="nav-bar">
+            <div className="logo-and-title">
+                <img className="small-logo" src="images/react-icon-small.png" />
+                <h1 className="title">ReactFacts</h1>    
+            </div>
+            <h3 className="extra-title">React Course - Project 1</h3>
+        </div>
     )
 }
